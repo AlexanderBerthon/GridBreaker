e853752d1adb0838413320bc6b22ac7c0774e15f
Alex Berthon <Alexjberthon@gmail.com>
2022-03-02 09:19:49 -0800

    Repopulate() Created function that replaces empty / cleared squares with colored squares. When this function is called is a gameplay decision that I haven't made yet.

--- Form1.cs
+++ Form1.cs
@@ -1,22 +1,57 @@
 namespace GridBreaker {
+
+    /// <summary>
+    /// goal of the game is to get as many points as possible in the given turns you have
+    /// the board will repopulate cells a limited number of times
+    /// to get the most points, the player has to plan ahead to try to completely clear the board
+    /// bonus points the larger the chain?
+    /// negative points for left over boxes?
+    /// is there a way to prevent user from clicking combo's less than 3?
+    /// or maybe just offer low points
+    /// or negative points?
+    /// </summary>
+
     public partial class Form1 : Form {
         //global variables :(
         Button[] btnArray = new Button[100];
-        
+        Random random = new Random();
+        int count = 50;
+
+
         public Form1() {
             InitializeComponent();
             flowLayoutPanel1.Controls.CopyTo(btnArray, 0);
+
+            //randomly iterate through all buttons and assign them a random color?
+            foreach(Button btn in btnArray) {
+                int color = random.Next(1, 11);
+                switch (color) {
+                    case 1: btn.BackColor = Color.Red; break;
+                    case 2: btn.BackColor = Color.Red; break;
+                    case 3: btn.BackColor = Color.Red; break;
+                    case 4: btn.BackColor = Color.Green; break;
+                    case 5: btn.BackColor = Color.Green; break;
+                    case 6: btn.BackColor = Color.Green; break;
+                    case 7: btn.BackColor = Color.Blue; break;
+                    case 8: btn.BackColor = Color.Blue; break;
+                    case 9: btn.BackColor = Color.Blue; break;
+                    case 10: btn.BackColor = Color.Yellow; break;
+                }
+            }
         }
 
 
         private void grid_Click(object sender, EventArgs e) {
-            //when clicked, run recursive function
-            //get color of the cell, via tag (R, G, B, P)
-            //
+
+
+
+            count--;
             Button clicked = (Button)sender;
             destroy(clicked);
             adjust();
-            repopulate();
+            if (count > 0 && count%5 == 0) {
+                repopulate();
+            }
         }
 
         private void destroy(Button cell) {
@@ -69,7 +104,50 @@ namespace GridBreaker {
         }
 
         private void repopulate() {
+            Boolean populating = true;
+            while (populating) {
+                populating = false;
+                foreach (Button btn in btnArray) {
+                    if (btn.BackColor == Color.White) {
+                        populating = true;
+                        int color = random.Next(1, 11);
+                        switch (color) {
+                            case 1: btn.BackColor = Color.Red; break;
+                            case 2: btn.BackColor = Color.Red; break;
+                            case 3: btn.BackColor = Color.Red; break;
+                            case 4: btn.BackColor = Color.Green; break;
+                            case 5: btn.BackColor = Color.Green; break;
+                            case 6: btn.BackColor = Color.Green; break;
+                            case 7: btn.BackColor = Color.Blue; break;
+                            case 8: btn.BackColor = Color.Blue; break;
+                            case 9: btn.BackColor = Color.Blue; break;
+                            case 10: btn.BackColor = Color.Yellow; break;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void repopulate_Old() {
+            for (int i = 0; i < 10; i++) {
+                int color = random.Next(1, 11);
+                switch (color) {
+                    case 1: btnArray[i].BackColor = Color.Red; break;
+                    case 2: btnArray[i].BackColor = Color.Red; break;
+                    case 3: btnArray[i].BackColor = Color.Red; break;
+                    case 4: btnArray[i].BackColor = Color.Green; break;
+                    case 5: btnArray[i].BackColor = Color.Green; break;
+                    case 6: btnArray[i].BackColor = Color.Green; break;
+                    case 7: btnArray[i].BackColor = Color.Blue; break;
+                    case 8: btnArray[i].BackColor = Color.Blue; break;
+                    case 9: btnArray[i].BackColor = Color.Blue; break;
+                    case 10: btnArray[i].BackColor = Color.Yellow; break;
+                }
+            }
 
         }
+
+
     }
 }
